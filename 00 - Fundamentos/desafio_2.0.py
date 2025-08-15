4c3113184c7bd28242fd8a26383e6a0d24924355
Lucas <klmbs.santos@gmail.com>
2025-08-15 16:05:10 -0300

    Update desafio_2.0.py

--- 00 - Fundamentos/desafio_2.0.py
+++ 00 - Fundamentos/desafio_2.0.py
@@ -1,34 +1,35 @@
+saldo = 0
+limite = 500
+extrato = []
+numero_saques = 0
+LIMITE_SAQUES = 3
 
-menu = """
+continuar = 1
+
+while continuar == 1:
+
+    menu = int(input('''
 
 [1] Depositar
 [2] Sacar
 [3] Extrato
-[q] Sair
 
-=> """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+=> '''))
 
-while True:
+    transformacao = str(menu)
 
-    opcao = input(menu)
-
-    if opcao == "1":
+    if transformacao == "1":
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:
             saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-
+            extrato.append(f"Depósito: R$ {valor:.2f}")
+            print('Depósito realizando.')
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "2":
+    elif transformacao == "2":
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
@@ -48,20 +49,34 @@ while True:
 
         elif valor > 0:
             saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
+            extrato.append(f"Saque: R$ {valor:.2f}")
             numero_saques += 1
-
+            print("Saque realizado.")
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "3":
+    elif transformacao == "3":
         print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
+        if extrato:
+            print("\n".join(extrato))
+        else:
+            print("Não foram realizadas movimentações.")
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
-    elif opcao == "q":
-        break
-
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+    continuar = int(input('''
+Voçê quer fazer mais alguma operação?
+
+[1] Sim
+[2] Não
+
+=> '''))
+
+    if continuar == 2:
+        print("Saindo... Obrigado por usar nosso banco!")
+    elif continuar != 1:
+        print("Opção inválida. Saindo por segurança.")
+        break
